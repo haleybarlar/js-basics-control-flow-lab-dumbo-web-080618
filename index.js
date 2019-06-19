fac9a162afda64c564ed5a4353b04946e6c7c3db
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,14 @@
 // switchOnCharmFromTip() — Use a switch statement to return a different response based on the generosity of the passenger's tip.
 
 
-scuberGreetingForFeet(ride) {
-  if (ride <= 400) {
-    return 'This one is on me!';
+// Write your code in this file!
+
+function scuberGreetingForFeet(ride) {
+  if (ride > 2500) {
+    return 'No can do.'
   } else if (ride > 2000) {
-    return 'I will gladly take your thirty bucks.';
-  } else if (ride > 2500) {
-    return 'No can do.';
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride <= 400) {
+    return 'This one is on me!'
   }
 }
