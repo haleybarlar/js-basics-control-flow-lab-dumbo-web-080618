e4961de855fc01577a588eb87707fa417f08aabe
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,13 @@ function ternaryCheckCity(city) {
     return 'No go.'
   }
 }
+
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
+}
