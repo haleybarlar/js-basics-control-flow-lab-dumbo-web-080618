e21215c4e6125264c76c83d33adb7adb9908709d
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ scuberGreetingForFeet(ride) {
   if (ride > 2500) {
     return 'No can do.';
   } else if (ride > 2000) {
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   } else if (ride <= 400) {
     return 'This one is on me!'
   }
